ce6c0af4d982cfa98a34e7e460753897e128b22f
abir781 <hasan.abir3176@gmail.com>
2026-05-04 12:20:07 +0600

    searchbox_added

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,9 +1,11 @@
+
 import Image from 'next/image';
 import React from 'react';
+import Searchbox from './Searchbox';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex justify-between max-w-10/12 mx-auto'>
 
             <div>
                 <Image src="/logo.png"
@@ -15,13 +17,28 @@ const Navbar = () => {
 
            <div>
             <ul>
-                <li>list1</li>
-                <li>list2</li>
-                <li>list3</li>
-                <li>list4</li>
+                <li>Women</li>
+                <li>Men</li>
+                <li>Kids</li>
+                <li>Homedecor</li>
             </ul>
            </div>
-            
+           <div>
+
+             <Searchbox></Searchbox>
+              
+           </div>
+
+          
+
+           {/* <div>
+               <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+           </div>
+             */}
         </div>
     );
 };
---
+++ app/Searchbox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Searchbox = () => {
+    return (
+        <div>
+
+            <input type='text' className='border-2 border-gray-500 w-[200px] h-[30px]'/>
+            
+        </div>
+    );
+};
+
+export default Searchbox;
