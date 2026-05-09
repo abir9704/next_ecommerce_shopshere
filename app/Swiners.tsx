3102eec0a301f53bf7b8b18a3aa0aa837ee537ff
abir781 <hasan.abir3176@gmail.com>
2026-05-09 11:44:39 +0600

    kjjj

--- app/Swiners.tsx
+++ app/Swiners.tsx
@@ -5,6 +5,8 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import deka from "next/font/local";
 
 // Import Swiper styles
 import 'swiper/css';
@@ -13,21 +15,31 @@ import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
 import Image from 'next/image';
 
+const myFont =  deka({
+  src: './font/deka.otf',
+  weight: '400',
+  style: 'normal'
+})
+
 const Swiners = () => {
   return (
      <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      modules={[Navigation, Pagination, Scrollbar, A11y,Autoplay]}
       spaceBetween={50}
       slidesPerView={1}
       navigation
+        autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
       <SwiperSlide>
-           <div className="relative h-[calc(100vh-96px)] w-full">
+           <div className={`relative h-[calc(100vh-96px)] w-full ${myFont.className}`}>
       <Image
         src="/1s.jpg"
         alt="Banner Image"
@@ -35,10 +47,14 @@ const Swiners = () => {
         className="object-cover"
         priority
       />
+      <div className="absolute inset-0 bg-black/60"></div>
+      <div className='flex justify-center items-center min-h-screen text-[#D4AF37]'>
+        <p className='text-5xl font-bold absolute'>Wear Confidence. Own the Moment.</p>
+      </div>
     </div>
       </SwiperSlide>
       <SwiperSlide>
-          <div className="relative h-[calc(100vh-96px)] w-full">
+          <div className={`relative h-[calc(100vh-96px)] w-full ${myFont.className}`}>
       <Image
         src="/2s.jpg"
         alt="Banner Image"
@@ -46,11 +62,16 @@ const Swiners = () => {
         className="object-cover"
         priority
       />
+      <div className="absolute inset-0 bg-black/60"></div>
+
+      <div className='flex justify-center items-center min-h-screen text-[#D4AF37]'>
+        <p className='text-5xl font-bold absolute'>Dress Bold. Live Bold.</p>
+      </div>
     </div>
       </SwiperSlide>
       <SwiperSlide>
 
-          <div className="relative h-[calc(100vh-96px)] w-full">
+          <div className={`relative h-[calc(100vh-96px)] w-full ${myFont.className}`}>
       <Image
         src="/3s.jpg"
         alt="Banner Image"
@@ -58,6 +79,11 @@ const Swiners = () => {
         className="object-cover"
         priority
       />
+       <div className="absolute inset-0 bg-black/60"></div>
+
+        <div className='flex justify-center items-center min-h-screen text-[#D4AF37]'>
+        <p className='text-5xl font-bold absolute'>Where Fashion Meets Confidence.</p>
+      </div>
     </div>
       </SwiperSlide>
       
